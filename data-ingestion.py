cb576c4b3ef23f92e7b5513b3419a23901f78062
AlpiGun5 <a.i.guncan02@gmail.com>
2025-02-10 03:21:52 +0100

    Add script for data ingestion
    - Only support MangaDex for now

---
+++ data-ingestion.py
@@ -0,0 +1,7 @@
+import os
+from mangadex_downloader import MangaDexDownloader
+
+manga_id = "efb4278c-a761-406b-9d69-19603c5e4c8b"
+downloader = MangaDexDownloader(manga_id)
+
+downloader.download_manga()
---
+++ mangadex_downloader.py
@@ -0,0 +1,62 @@
+import os
+import requests
+
+MANGADEX_API = "https://api.mangadex.org"
+
+
+class MangaDexDownloader:
+    def __init__(self, manga_id: str, save_path: str = "panels"):
+        self.manga_id = manga_id
+        self.save_path = save_path
+        os.makedirs(self.save_path, exist_ok=True)
+
+    def get_chapters(self):
+        url = f"{MANGADEX_API}/manga/{self.manga_id}/feed"
+        response = requests.get(
+            url,
+            params={
+                "translatedLanguage[]": "en",
+                "order[chapter]": "asc",
+                "limit": 500,
+            },
+        )
+        return response.json().get("data", []) if response.status_code == 200 else []
+
+    def get_pages(self, chapter_id: str):
+        url = f"{MANGADEX_API}/at-home/server/{chapter_id}"
+        response = requests.get(url)
+        if response.status_code == 200:
+            json = response.json()
+            return json.get("baseUrl") + "/data/" + json.get("chapter").get(
+                "hash"
+            ), json.get("chapter").get("data")
+        return "", []
+
+    def download_chapter(self, chapter):
+        chapter_id = chapter["id"]
+        chapter_number = chapter["attributes"].get("chapter", "unknown")
+        print(f"Downloading chapter {chapter_number}...")
+        base, pages = self.get_pages(chapter_id)
+        if not pages:
+            print(f"No pages found for chapter {chapter_number}")
+            return
+
+        chapter_path = os.path.join(self.save_path, f"chapter_{chapter_number}")
+        os.makedirs(chapter_path, exist_ok=True)
+
+        for page in pages:
+            page_url = f"{base}/{page}"
+            self.download_image(page_url, os.path.join(chapter_path, page))
+
+    def download_image(self, url, save_path):
+        response = requests.get(url, stream=True)
+        if response.status_code == 200:
+            with open(save_path, "wb") as f:
+                for chunk in response.iter_content(1024):
+                    f.write(chunk)
+            print(f"Downloaded {save_path}")
+        else:
+            print(f"Failed to download {url}")
+
+    def download_manga(self):
+        [self.download_chapter(chapter) for chapter in self.get_chapters()]
